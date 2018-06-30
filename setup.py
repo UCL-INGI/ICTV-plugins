89ed27b1101524b9e70e098a3bdd8f967d74b021
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-06-30 10:27:32 +0200

    Fixes setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,11 @@ setup(
     version=release,
     packages=['ictv.plugins.editor', 'ictv.plugins.embed', 'ictv.plugins.img-grabber', 'ictv.plugins.rss'],
     package_dir={'ictv': 'ictv'},
-    url='https://github.com/UCL-INGI/ICTV',
+    url='https://github.com/UCL-INGI/ICTV-plugins',
     license='GNU AGPL v3',
     author='Michel François, Piraux Maxime, Taffin Ludovic, Nicolas Detienne, Pierre Reinbold',
     author_email='',
     description='ICTV is a simple content management system for digital signage on multiple screens.',
     install_requires=['pyquery', 'BeautifulSoup4', 'feedparser'],
-    test_requires=['pytest', 'pytest-xdist', 'pytest-cov', 'paste', 'nose'],
     include_package_data=True,
 )
